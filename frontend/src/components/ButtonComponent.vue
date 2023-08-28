f984081ac7b3fb61db696122cb69561a6908bc35
Julio <juliocanizalez@outlook.com>
2023-08-28 00:56:24 -0600

    Add button component

---
+++ frontend/src/components/ButtonComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-btn
+    color="secondary"
+    :label="label! || 'Calcular'"
+    :type="isSubmit ? 'subtmit' : 'button'"
+  ></q-btn>
+</template>
+<script setup lang="ts">
+defineProps({
+  label: String,
+  isSubmit: Boolean,
+});
+</script>
--- frontend/src/components/EssentialLink.vue
+++ frontend/src/components/EssentialLink.vue
@@ -1,50 +1,41 @@
 <template>
-  <q-item clickable tag="a" @click="navigateTo">
-    <q-item-section v-if="icon" avatar>
-      <q-icon :name="icon" />
-    </q-item-section>
-
-    <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
-    </q-item-section>
-  </q-item>
+  <q-btn-dropdown
+    v-if="childLinks?.length"
+    flat
+    class="link-button"
+    :label="title"
+  >
+    <q-list>
+      <q-item
+        clickable
+        v-close-popup
+        v-for="(child, index) in childLinks!"
+        :key="index"
+        @click="navigateTo(child.link!)"
+      >
+        <q-item-section>
+          <q-item-label>{{ child.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
+  <q-btn v-else flat class="link-button" @click="navigateTo(props.link!)">
+    {{ title }}
+  </q-btn>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
 import { useRouter } from 'vue-router';
 
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-
-    caption: {
-      type: String,
-      default: '',
-    },
-
-    link: {
-      type: String,
-      default: '#',
-    },
-
-    icon: {
-      type: String,
-      default: '',
-    },
-  },
-  setup(props) {
-    const router = useRouter();
-    return {
-      navigateTo() {
-        router.push({ name: props.link });
-      },
-    };
-  },
+const props = defineProps({
+  title: String,
+  link: String,
+  childLinks: Array as () => Array<{ title: string; link: string }>,
 });
+
+const router = useRouter();
+
+const navigateTo = (link: string) => {
+  router.push({ name: link });
+};
 </script>
