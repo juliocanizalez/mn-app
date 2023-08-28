92003ce3798d7a58bc766ee76f17d53379c5be56
Julio <juliocanizalez@outlook.com>
2023-08-27 22:03:36 -0600

    Add routes

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,14 +1,43 @@
 import { RouteRecordRaw } from 'vue-router';
+import { routes as r } from 'src/utils';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue'),
+        name: 'home',
+      },
+      {
+        path: r.biseccion,
+        component: () => import('pages/BiseccionPage.vue'),
+        name: r.biseccion,
+      },
+      {
+        path: r.falsaPosicion,
+        component: () => import('pages/FalsaPosicionPage.vue'),
+        name: r.falsaPosicion,
+      },
+      {
+        path: r.newtonRaphson,
+        component: () => import('pages/NewtonRaphson.vue'),
+        name: r.newtonRaphson,
+      },
+      {
+        path: r.puntoFijo,
+        component: () => import('pages/PuntoFijoPage.vue'),
+        name: r.puntoFijo,
+      },
+      {
+        path: r.secante,
+        component: () => import('pages/SecantePage.vue'),
+        name: r.secante,
+      },
+    ],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
