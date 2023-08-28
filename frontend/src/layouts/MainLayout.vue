847e991ab284202431ea529aa18ee9d8f31180d3
Julio <juliocanizalez@outlook.com>
2023-08-28 00:57:05 -0600

    Update layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,31 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
+      <q-toolbar class="q-px-xl">
         <q-toolbar-title> Metodos Numericos - Portafolio </q-toolbar-title>
+        <EssentialLink title="Inicio" link="home" />
+        <EssentialLink title="Periodo I" :child-links="linksList" />
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Listado de Ejercicios </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -33,61 +14,30 @@
   </q-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
+<script setup lang="ts">
 import EssentialLink from 'components/EssentialLink.vue';
 import { routes } from 'src/utils';
 
 const linksList = [
   {
-    title: 'Inicio',
-    icon: 'home',
-    link: 'home',
-  },
-  {
     title: 'Biseccion',
-    icon: ' ',
     link: routes.biseccion,
   },
   {
     title: 'Falsa Posicion',
-    icon: ' ',
     link: routes.falsaPosicion,
   },
   {
     title: 'Newton-Raphson',
-    icon: ' ',
     link: routes.newtonRaphson,
   },
   {
     title: 'Punto Fijo',
-    icon: ' ',
     link: routes.puntoFijo,
   },
   {
     title: 'Secante',
-    icon: ' ',
     link: routes.secante,
   },
 ];
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink,
-  },
-
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-    };
-  },
-});
 </script>
