a6eb807e093fde00e1f2f0961b58191c857f87a9
Julio <juliocanizalez@outlook.com>
2023-08-27 22:02:08 -0600

    Format file

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -29,15 +29,10 @@ module.exports = configure((/* ctx */) => ({
   // app boot file (/src/boot)
   // --> boot files are part of "main.js"
   // https://v2.quasar.dev/quasar-cli-vite/boot-files
-  boot: [
-
-    'axios',
-  ],
+  boot: ['axios'],
 
   // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-  css: [
-    'app.sass',
-  ],
+  css: ['app.sass'],
 
   // https://github.com/quasarframework/quasar/tree/dev/extras
   extras: [
@@ -180,13 +175,11 @@ module.exports = configure((/* ctx */) => ({
 
     packager: {
       // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
       // OS X / Mac App Store
       // appBundleId: '',
       // appCategoryType: '',
       // osxSign: '',
       // protocol: 'myapp://path',
-
       // Windows only
       // win32metadata: { ... }
     },
@@ -200,9 +193,7 @@ module.exports = configure((/* ctx */) => ({
 
   // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
   bex: {
-    contentScripts: [
-      'my-content-script',
-    ],
+    contentScripts: ['my-content-script'],
 
     // extendBexScriptsConf (esbuildConf) {}
     // extendBexManifestJson (json) {}
