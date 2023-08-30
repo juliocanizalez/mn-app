13a741f355bf133fa5c83ada9ab9341e9125b4c5
Julio <juliocanizalez@outlook.com>
2023-08-30 14:43:32 -0600

    Add dummy index, doesnt matter at this point

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,5 +1,10 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="column items-center justify-center bg-grey-1 text-focus-in">
+    <p class="text-weight-medium text-h2 text-center">
+      Aplicacion de Metodos Numericos en la Computacion
+    </p>
+    <p class="text-subtitle1 text-center">Julio Eduardo Canizalez Salinas</p>
+  </q-page>
 </template>
 
 <script setup lang="ts"></script>
